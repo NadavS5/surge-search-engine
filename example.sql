59614bbe56370e6844cbe7ce7ea4e36fe22721a1
NadavS5 <electric5008@gmail.com>
2024-08-14 17:56:55 +0300

    sqlite tables  + indices

---
+++ example.sql
@@ -0,0 +1,16 @@
+INSERT INTO websites (url,title,keywords) VALUES ("google.com","google","search engine google ")
+
+INSERT INTO keywords (keyword) VALUES ("google") --Keywords--
+INSERT INTO keywords (keyword) VALUES ("search")
+INSERT INTO keywords (keyword) VALUES ("engine")
+
+INSERT INTO keyword_website_map (keyword_id,website_id,relevance_score) VALUES (1,1,1) --Keyword Id Website Id Relevance score--
+
+
+
+-- SELECT d.url, d.title, kdm.relevance_score
+-- FROM keyword_document_map kdm
+-- JOIN keywords k ON kdm.keyword_id = k.id
+-- JOIN documents d ON kdm.document_id = d.id
+-- WHERE k.keyword = 'example'
+-- ORDER BY kdm.relevance_score DESC;
--- sqlite.py
+++ sqlite.py
@@ -7,26 +7,34 @@ connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
 
 
-#cursor.execute('CREATE TABLE IF NOT EXISTS websites (url TEXT UNIQUE, title TEXT, keywords TEXT)')
-cursor.execute('DELETE FROM websites WHERE 1=1')
+#cursor.execute('CREATE TABLE IF NOT EXISTS websites (id INTEGER PRIMARY KEY AUTOINCREMENT ,url TEXT UNIQUE, title TEXT, keywords TEXT)')
+#cursor.execute('DELETE FROM websites WHERE 1=1')
 
 #cursor.execute('CREATE TABLE keywords (id INTEGER PRIMARY KEY AUTOINCREMENT,keyword TEXT NOT NULL UNIQUE')
 #cursor.execute(create_keyword_document_map_quary)
 
+
+create_keywords_table_quary =(
+    """CREATE TABLE keywords (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        keyword TEXT NOT NULL UNIQUE
+    )""")
+
 create_keyword_document_map_quary =( 
     """
-    CREATE TABLE keyword_document_map (
+    CREATE TABLE keyword_website_map (
     keyword_id INTEGER,
-    document_id INTEGER,
+    website_id INTEGER,
     relevance_score REAL,
     FOREIGN KEY (keyword_id) REFERENCES keywords(id),
-    FOREIGN KEY (document_id) REFERENCES documents(id),
-    PRIMARY KEY (keyword_id, document_id)"""
+    FOREIGN KEY (website_id) REFERENCES websites(id),
+    PRIMARY KEY (keyword_id, website_id))"""
     )#explenation: creates object that keywords stores website's specific keyword evaluation 
     #(FOREIGN KEY (output) REFERENCES table(field)= 
     #stores the primary key of table(field) in output  )
 
-
+#cursor.execute(create_keywords_table_quary)
+#cursor.execute(create_keyword_document_map_quary)
 
 # with open('urls.txt','r') as f:
 #     for line in f.readlines():
